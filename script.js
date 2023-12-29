a3df5f63145861798a2273cbbe0ffcb059b136fc
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-29 18:11:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+// Function to parse the encoded string
 const parseCode = (str) => {
-  // your code here
+  // Split the input string using "000" as the separator
+  const parts = str.split("000");
+
+  // Extract the values for firstName, lastName, and id
+  const firstName = parts[0];
+  const lastName = parts[1];
+  const id = parts[2];
+
+  // Create and return the result object
+  const result = {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
+
+  return result;
 };
 
-// Do not change the code below
+// Get user input using prompt
 const str = prompt("Enter str: ");
+
+// Parse the input string and display the result
 alert(JSON.stringify(parseCode(str)));
+
